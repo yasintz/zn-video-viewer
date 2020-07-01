491e8f43fe85a1d01893a8c1c41966122e6db03f
yasintz <yasinntazeoglu@gmail.com>
2020-07-01 16:22:15 +0300

    updated files

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-const port = process.env.PORT || 3000
-const morgan = require('morgan')
+const port = process.env.PORT || 3000;
+const morgan = require('morgan');
 
-var express = require('express')
-var app = express()
-app.use(express.json())
-app.use(morgan('dev'))
+var express = require('express');
+var app = express();
+app.use(express.json());
+app.use(morgan('dev'));
 
-app.listen(port, err => {
-  if (err) throw err
-  console.log(`> Ready on server http://localhost:${port}`)
-})
+app.listen(port, (err) => {
+  if (err) throw err;
+  console.log(`> Ready on server http://localhost:${port}`);
+});
 
 app.get('/', (req, res) => {
-  res.send('ExpressJS server is online.')
-})
+  res.send('ExpressJS server is online.');
+});
 
 app.get('/get', (req, res, next) => {
   res.json({
     version: process.env.VERSION,
-  })
-})
+    yasin: true,
+  });
+});
 
-app.post('/post', function(request, response) {
-  response.send(request.body)
-})
+app.post('/post', function (request, response) {
+  response.send(request.body);
+});
